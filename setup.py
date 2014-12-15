5407aced7f66b0cb073fc3231279ea412082368b
黄毅 <yi.codeplayer@gmail.com>
2014-12-15 17:47:04 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name = 'clogging',
+    version = '1.0.0',
+    packages=['clogging'],
+    author='huangyi',
+    author_email='yi.codeplayer@gmail.com',
+    url = 'https://github.com/yihuang/clogging',
+    description = 'A simplified and faster logging module for python',
+)
